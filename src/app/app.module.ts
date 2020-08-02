a6424472f9c42d4495b96f8aad1077c140805c1e
gyu_horin <gyu.horin@gmail.com>
2020-08-02 20:50:41 +0900

    2リスト表示OK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import {RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
   ],
   imports: [
-    BrowserModule
+        BrowserModule,
+        RouterModule,
+        FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
